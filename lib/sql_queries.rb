4f78918da78e8a7bea9e536f0f68bd49d6f62632
Matthew Collins <matthew.collins1991@yahoo.com>
2019-01-16 13:19:52 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -30,9 +30,9 @@ HAVING SUM(pledges.amount) - projects.funding_goal >= 0"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, SUM(pledges.amount) - projects.funding_goal
+"SELECT users.name, SUM(pledges.amount)
 FROM projects JOIN pledges
-WHERE projects.id = pledges.project_id
+WHERE users.id = pledges.project_id
 GROUP BY projects.title
 HAVING SUM(pledges.amount) - projects.funding_goal >= 0"
 end
