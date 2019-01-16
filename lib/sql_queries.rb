933125232ddcf912d3ba7d131a14ef6ec5529624
Matthew Collins <matthew.collins1991@yahoo.com>
2019-01-16 11:29:41 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -14,8 +14,8 @@ GROUP BY projects.title"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT users.name, pledges.amount
-FROM users
+"SELECT users.name, users.age, pledges.amount
+FROM users JOIN pledges
 ORDER BY users.name ASC"
 end
 
