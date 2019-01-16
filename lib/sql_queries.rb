bff1e6aaeac5115437215d30cceee14b1dca8bee
Matthew Collins <matthew.collins1991@yahoo.com>
2019-01-16 13:39:05 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -41,8 +41,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 "SELECT projects.category, SUM(pledges.amount)
 FROM projects JOIN pledges
 WHERE projects.id = pledges.project_id
-GROUP BY pledges.amount
-HAVING projects.category = 'music'"
+WHERE projects.category = 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
