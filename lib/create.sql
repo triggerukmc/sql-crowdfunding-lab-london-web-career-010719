df860b61c4dafdf6dc4f1a88b7feee0ddec58abe
Matthew Collins <matthew.collins1991@yahoo.com>
2019-01-16 11:14:07 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,10 +1,9 @@
 CREATE TABLE projects(
 			id INTEGER PRIMARY KEY,
 			title TEXT
-			catagory INTEGER
-			funding_goal TEXT
+			catagory TEXT
+			funding_goal INTEGER
 			start_date TEXT
 			end_date TEXT
 			);
 
-id, title, category, funding_goal, start_date, end_date
