b80fb0b631b7ee3319449a71092a44a51d2a9b6d
Matthew Collins <matthew.collins1991@yahoo.com>
2019-01-16 13:27:35 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -38,7 +38,11 @@ ORDER BY SUM(pledges.amount), users.name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT users.name, SUM(pledges.amount)
+FROM users JOIN pledges
+WHERE users.id = pledges.user_id
+GROUP BY users.name
+ORDER BY SUM(pledges.amount), users.name"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
