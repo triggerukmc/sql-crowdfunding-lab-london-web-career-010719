ba3912db9b89688ce9c29cd4a45a78e1570b3324
Matthew Collins <matthew.collins1991@yahoo.com>
2019-01-16 11:36:43 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT projects.title, SUM(pledges.amount)
 FROM projects JOIN pledges
-ON projects.id = pledges.project_id
+WHERE projects.id = pledges.project_id
 GROUP BY projects.title"
 end
 
